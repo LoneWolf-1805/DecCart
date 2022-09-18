5981245bb49e44782834c6dff014ad59ea1e4d9f
LoneWolf-1805 <74724048+LoneWolf-1805@users.noreply.github.com>
2022-09-18 10:54:08 +0530

    Update Sidebar.js

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -4,22 +4,10 @@ import { Image } from 'next/image'
 
 const Sidebar = () => {
   return (
-    <div className='styles.container'>
-      <div className='styles.profile'>
-        {
-          isAuthenticated && (
-            <div>
-              <Image alt='profile' className='styles.ProfilePic' height={100} width={100}/>
-
-            </div>
-            {!username ? (
-              
-            )}
-          )
-        }
-
+    <div>
+      <div>
+      Hello there!!
       </div>
-
       <ConnectButton/>
     </div>
   )
