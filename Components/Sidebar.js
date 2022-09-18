bcd496d5e855907e3b894e2ed4e2a73b2d4e6cd4
LoneWolf-1805 <74724048+LoneWolf-1805@users.noreply.github.com>
2022-09-18 10:48:50 +0530

    Update Sidebar.js

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -2,22 +2,12 @@ import React from 'react'
 import { ConnectButton } from 'web3uikit'
 import { Image } from 'next/image'
 
+
 const Sidebar = () => {
   return (
-    <div className='styles.container'>
-      <div className='styles.profile'>
-        {
-          isAuthenticated && (
-            <div>
-              <Image alt='profile' className='styles.ProfilePic' height={100} width={100}/>
-
-            </div>
-            {!username ? (
-              
-            )}
-          )
-        }
-
+    <div >
+      <div >
+        hello
       </div>
 
       <ConnectButton/>
